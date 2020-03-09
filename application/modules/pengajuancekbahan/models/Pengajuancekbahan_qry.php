efa0798610aa11e302ce6a29eb23b6fa9e8b17f1
Syafrial Zulmi <syafrialzulmi@gmail.com>
2020-03-09 15:09:53 +0700

    update

--- application/modules/pengajuancekbahan/models/Pengajuancekbahan_qry.php
+++ application/modules/pengajuancekbahan/models/Pengajuancekbahan_qry.php
@@ -21,11 +21,16 @@ class Pengajuancekbahan_qry extends CI_Model {
                         , z.tanggalpengajuan
                         , sum(z.totalhargatotal) as hargatotal		
                         from (
-                                        SELECT y.idpengajuan, y.tanggalrekap, y.tanggalpengajuan
+                            SELECT aa.idpengajuan, aa.tanggalrekap, aa.tanggalpengajuan,
+                                    aa.idbahan, aa.idbahansupplier, aa.namabahan, aa.satuan,
+                                    aa.hargasatuansupplier, aa.satuansupplier,( aa.jumlahkuantitas - IFNULL(bb.jumlahkuantitas, 0)) AS jumlahkuantitas,
+                                    (aa.jumlahkuantitas - IFNULL(bb.jumlahkuantitas, 0)) * aa.hargasatuansupplier AS totalhargatotal,
+                                    aa.idpengajuandiet,bb.idsisabahan
+                                FROM ( SELECT y.idpengajuan, y.tanggalrekap, y.tanggalpengajuan
                                             , y.idbahan, y.idbahansupplier
                                             , y.namabahan, y.satuan
                                             , y.hargasatuansupplier, y.satuansupplier
-                                            , (y.jumlahkuantitas-IFNULL(x.jmlkuantitaspengurangan,0)) AS totaljumlahkuantitas
+                                            , (y.jumlahkuantitas-IFNULL(x.jmlkuantitaspengurangan,0)) AS jumlahkuantitas
                                             , (y.jumlahkuantitas-IFNULL(x.jmlkuantitaspengurangan,0))*y.hargasatuansupplier AS totalhargatotal
                                             , x.idpengajuan AS idpengajuandiet
                                         FROM ( SELECT  a.idpengajuan, a.tanggalrekap, a.tanggalpengajuan
@@ -51,7 +56,8 @@ class Pengajuancekbahan_qry extends CI_Model {
                                                 GROUP BY z.idpengajuan, z.tanggalrekap, z.tanggalpengajuan, z.idbahan, z.namabahan, z.satuan) AS x ON y.idpengajuan = x.idpengajuan
                                                     AND y.tanggalpengajuan = x.tanggalpengajuan
                                                     AND y.tanggalrekap = x.tanggalrekap
-                                                    AND y.idbahan = x.idbahan ) as z
+                                                    AND y.idbahan = x.idbahan) AS aa
+                        LEFT OUTER JOIN sisabahan AS bb ON aa.idpengajuan = bb.idpengajuan AND aa.idbahan = bb.idbahan ) as z
                         group by z.tanggalpengajuan
                     union all
                     select y.stat
@@ -62,6 +68,7 @@ class Pengajuancekbahan_qry extends CI_Model {
                                         , sum(b.hargatotalreal) as hargatotalreal
                                         , 'pengecekan' as stat
                                 from pengajuanbahan as b
+                                where b.kesesuaian <> 'batal'
                                 group by b.namabahan, b.tanggalpengajuan) as y
                     group by y.tanggalpengajuan) as bahanmasakan";
         
